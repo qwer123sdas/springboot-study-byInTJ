a4a5418f334342a90f05293062d4ebaa8111512e
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-08-03 19:26:17 +0900

    프록시와 스코프

--- src/main/java/hello/core/common/MyLogger.java
+++ src/main/java/hello/core/common/MyLogger.java
@@ -1,6 +1,7 @@
 package hello.core.common;
 
 import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -8,7 +9,7 @@ import javax.annotation.PreDestroy;
 import java.util.UUID;
 
 @Component
-@Scope(value = "request") // HTTP 요청 당 하나씩 생성하고 요청이 끝날때 소멸
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS) // HTTP 요청 당 하나씩 생성하고 요청이 끝날때 소멸
 public class MyLogger {
     private String uuid;
     private String requestURL;
