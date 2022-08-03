42cf12c6813983cdb868532e8e6782b856017e5e
violin0117 <99157631+qwer123sdas@users.noreply.github.com>
2022-08-03 17:45:29 +0900

    웹 실행을 통해 request scope에 따른 서로다른 객체 확인

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,36 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request") // HTTP 요청 당 하나씩 생성하고 요청이 끝날때 소멸
+public class MyLogger {
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message){
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "]" + "{" + message + "}");
+    }
+
+    @PostConstruct
+    public void init(){
+        // 이 빈이 생성되는 시점에서 HTTP요청 당 하나씩 생성되므로 uuid를 통해 다른 HTTP요청과 구분가능
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create : " + this);
+
+    }
+
+    @PreDestroy
+    public void close(){
+        System.out.println("[" + uuid + "] request scope bean close : " + this);
+    }
+}
